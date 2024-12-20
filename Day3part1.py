e76bbbbf0750bd984b85b2a7e9bbfa0044b7233a
Yktrasdi <132462495+Yktrasdi@users.noreply.github.com>
2024-12-20 08:48:24 +0100

    Day3part1 funzionante

--- Day3part1.py
+++ Day3part1.py
@@ -1,19 +1,16 @@
 import re
 f=open("input.txt","r")
+str=f.readline()
 str1=""
 for line in f:
     str1+=str(line)
 arrMul=[]
-arrMul=(re.findall(r"mul\(\d{1,3}\,\d{1,3}\)|do\(\)|don't\(\)",str1))
+arrMul.append(re.findall(r"mul\(\d{1,3}\,\d{1,3}\)",str))
+print(arrMul)
+arrMul=(re.findall(r"mul\(\d{1,3}\,\d{1,3}\)",str1))
 sum1=0
-do=True
 for x in arrMul:
-    if(x=="don't()"):
-        do=False
-    elif(x=="do()"):
-        do=True
-    else:
-        if(do):
-            nums=re.findall(r"\d+",x)
-            sum1+=int(nums[0])*int(nums[1])
+    nums=re.findall(r"\d+",x)
+    print(nums)
+    sum1+=int(nums[0])*int(nums[1])
 print(sum1)
