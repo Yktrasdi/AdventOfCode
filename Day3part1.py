a3665e7a87036fe874f970e68f56c67522dee667
Yktrasdi <132462495+Yktrasdi@users.noreply.github.com>
2024-12-20 08:34:57 +0100

    Day3part1 funzionante

--- Day3part1.py
+++ Day3part1.py
@@ -1,6 +1,13 @@
 import re
 f=open("input.txt","r")
-str=f.readline()
+str1=""
+for line in f:
+    str1+=str(line)
 arrMul=[]
-arrMul.append(re.findall(r"mul\(\d{1,3}\,\d{1,3}\)",str))
-print(arrMul)
+arrMul=(re.findall(r"mul\(\d{1,3}\,\d{1,3}\)",str1))
+sum1=0
+for x in arrMul:
+    nums=re.findall(r"\d+",x)
+    print(nums)
+    sum1+=int(nums[0])*int(nums[1])
+print(sum1)
